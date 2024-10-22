7417425641b58dd27d685bd15a8825efbcb91286
Emmanuel Orozco <indi3.rok@gmail.com>
2024-10-22 03:42:23 +0200

    test

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
-      // NextJS <Image> component needs to whitelist domains for src={}
       "lh3.googleusercontent.com",
       "pbs.twimg.com",
       "images.unsplash.com",
@@ -11,7 +10,6 @@ const nextConfig = {
     ],
   },
   experimental: {
-    appDir: true,
     serverComponentsExternalPackages: ["mdpdf", "pdfjs-dist"],
   },
 };
